8b2a8bb650963dffd596ead726f300ec5ce952c0
Evan Lake <evanlake583@gmail.com>
2021-07-08 08:37:27 -0700

    tweaked jumping and added obsticles

--- src/prefabs/Obsticle.js
+++ src/prefabs/Obsticle.js
@@ -1,22 +1,17 @@
-//Spaceship prefab
+//Obsticle prefab
 class Obsticle extends Phaser.GameObjects.Sprite {
-    constructor(scene, x, y, texture, frame, pointValue) {
+    constructor(scene, x, y, texture, frame, speed) {
         super(scene, x, y, texture, frame);
         scene.add.existing(this);
-        this.points = pointValue;
-        this.moveSpeed = game.settings.spaceshipSpeed;
+        this.moveSpeed = speed;
     }
 
     update() {
         // moves left
         this.x -= this.moveSpeed;
-        //wrap around left to right
-        if (this.x <= 0 - this.width) {
-            this.x  = game.config.width;
-        }
     }
 
     reset() {
-        this.x  = game.config.width;
+        this.x = game.config.width;
     }
 }
